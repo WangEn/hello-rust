f3d96aaea0ccf04b3a8c7b254fd2c1106dbcca6f
Warn <imwarn@163.com>
2022-10-17 17:40:30 +0800

    Rust: 2-1 解构式变量

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-fn main() {
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
-
-    // let _a = 5;
-    // let b = 6;
-
-    let (a, mut b): (bool, bool) = (true, false);
-    println!("a = {:?}, b = {:?}", a, b);
-    b = true;
-    assert_eq!(a, b);
-    // println!("{:?}", assert_eq!(a, b));
-
+struct Struct {
+    e: i32
 }
 
+fn main() {
+    // 常量: 必须标注类型
+    const MAX_POINTS: u32 = 100_000;
+    // 解构式赋值
+
+    let (a, b, c, d, e);
+
+    (a, b) = (1, 2);
+
+    [c, .., d, _] = [1, 2, 3, 4, 5];
+
+    Struct { e, .. } = Struct {e: 5};
+
+    assert_eq!([1, 2, 1, 4, 5], [a, b, c, d, e]);
+}
 
 // 2-1 Rust变量绑定与解绑 Tips
 // Rust变量默认是不可变的，设置mut关键字将其变为可变的
