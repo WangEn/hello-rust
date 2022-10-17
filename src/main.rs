93d4fae19f93ce22734c5953c024d05556f4702f
Warn <imwarn@163.com>
2022-10-17 18:27:47 +0800

    Rust: 2-2-1 数值类型-浮点型之经典的0.1+0.2≠0.3精度问题

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
 fn main() {
-    // shadowing 变量遮蔽
-    let x = 5;
-    let x = x + 1;
+    // 二进制精度问题，导致0.1+0.2并不严格等于0.3，可能在小数点N位后存在误差
+    // assert!(0.1 + 0.2 == 0.3);
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
-    println!("The value of x is: {}", x);
+    // 折中的判断方式
+    // assert!((0.1_f64 + 0.2 - 0.3).abs() < 0.00001);
 
-    // 是用mut设置为可变变量时，会因为数值类型不匹配报错
-    // let mut spaces = "   ";
-    // spaces = spaces.len();
-    let spaces = "    ";
-    let spaces = spaces.len();
-    
-    println!("spaces's length is {}", spaces);
+    // 震撼灵魂的精度比较
+    let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
+    let xyz: (f64, f64, f64) = (0.1, 0.2, 0.3);
+
+    println!("abc (f32)");
+    println!("   0.1 + 0.2: {:x}", (abc.0 + abc.1).to_bits());
+    println!("         0.3: {:x}", (abc.2).to_bits());
+    println!();
+
+    println!("xyz (f64)");
+    println!("   0.1 + 0.2: {:x}", (xyz.0 + xyz.1).to_bits());
+    println!("         0.3: {:x}", (xyz.2).to_bits());
+    println!();
+
+    assert!(abc.0 + abc.1 == abc.2);
+    assert!(xyz.0 + xyz.1 == xyz.2);
 }
 
-// 2-1 Rust变量绑定与解绑 Tips
-// Rust变量默认是不可变的，设置mut关键字将其变为可变的
-// 使用下划线作为变量名的开头，使Rust忽略未使用变量的警告
-// const定义常量，定义后不可变，定义时必须标注类型，绑定值
-// 变量遮蔽：允许声明相同的变量名，在后面声明的变量会遮蔽掉前面声明的，节省变量名
+// 2-2 Rust基本类型 Tips
+// 2-2-1 数值类型
+// 浮点型准则：
+// 避免在浮点数上测试相等性
+// 当结果在数学上可能存在未定义时，需要格外的小心
