497b25680f2003aa097464188b5b0e551265b0ef
Warn <imwarn@163.com>
2022-10-17 17:13:21 +0800

    Rust: 2-0基础入门

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,18 @@
 fn main() {
-    let penguin_data = "\
-   common name,length (cm)
-   Little penguin,33
-   Yellow-eyed penguin,65
-   Fiordland penguin,60
-   Invalid,data
-   ";
-    let records = penguin_data.lines();
-    for (i, record) in records.enumerate() {
-        if i == 0 || record.trim().len() == 0 {
-            continue;
-        }
+    let a = 10;
+    let b: i32 = 20;
+    // mutable 可变的c
+    let mut c = 30i32;
+    let d = 30_i32;
+    let e = add(add(a, b), add(c, d));
 
-        let fields: Vec<_> = record.split(',').map(|field| field.trim()).collect();
-        if cfg!(debug_assertions) {
-            eprintln!("debug:{:?} -> {:?}", record, fields);
-        }
-        let name = fields[0];
-        if let Ok(length) = fields[1].parse::<f32>() {
-            println!("{}, {}cm", name, length);
-        }
-    }
+    println!("( a + b ) + (c + d) = {}", e);
 }
+
+fn add(i: i32, j: i32) -> i32 {
+    return i + j;
+}
+
+// 2-0 Rust基础入门 Tips
+// 字符串使用双引号 "" 而不是单引号 ''，Rust 中单引号是留给单个字符类型（char）使用的
+// Rust 使用 {} 来作为格式化输出占位符，其它语言可能使用的是 %s，%d，%p 等，由于 println! 会自动推导出具体的类型，因此无需手动指定
