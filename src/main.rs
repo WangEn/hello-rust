17a3ab35756c08e918443a597917eac522af070d
Warn <imwarn@163.com>
2022-10-18 15:03:39 +0800

    Rust: 2-3所有权与借用

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,11 @@
-fn plus_five(x:i32) -> i32 {
-    x + 5
-}
-
 fn main() {
-    let x = plus_five(5);
-
-    println!("The value of x is: {}", x);
-    // 崩溃函数
-    panic!("I return nothing, too!")
-    // 循环函数
-    // never_return()
+    println!("所有权")
 }
 
-// 永不返回的循环函数
-use std::thread;
-use std::time;
+// 2-3 所有权与借用 Tips
+// 2-3-1 所有权
 
-fn never_return() -> !{
-    loop {
-        println!("I return nothing");
-        thread::sleep(time::Duration::from_secs(1))
-    }
-}
-
-// 2-2 Rust基本类型 Tips
-// 2-2-4 函数
-
-// ⭐️函数要点
-// 函数名和变量名使用蛇形命名法，即下划线连接
-// 函数的位置可以随便放
-// 每个函数参数都需要标注类型，缺少任一个都会报错
-
-// ⭐️函数返回
-// 函数的返回值默认就是函数体最后一条表达式的返回值
-// 也可以使用return 表达式 提前返回
-
-// ⭐️特殊返回类型
-// 无返回值()
-
-// 永不返回的发散函数 diverge function，特别的，这种语法往往用做会导致程序崩溃的函数：
-// fn dead_end() -> ! {
-//     panic!("你已经到了穷途末路，崩溃吧！");
-// }
-
-// and 无限循环，永不跳出的函数
+// ⭐️编程语言释放内存的流派
+// 垃圾回收机制（GC） - Java Go
+// 手动管理内存的分配和释放 C++
+// 通过所有权来管理内存 Rust
