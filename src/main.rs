c833b12283313788a4e6001b3d93ef1c36d4f723
Warn <imwarn@163.com>
2022-10-19 18:47:41 +0800

    Rust: 2-3所有权与借用::引用和借用

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,28 @@
 fn main() {
-    let s = String::from("hello");
+    let mut s = String::from("hello");
 
-    takes_ownership(s);
+    change(&mut s);
 
-    // println!("{}", s);
-    // value borrowed here after move
-
-    let x = 5;
-    makes_copy(x);
-    println!("{}", x);
+    println!("{}", s);
 
 }
 
-fn takes_ownership(some_string: String) {
-    println!("{}", some_string);
+fn change(some_thing: &mut String) {
+    some_thing.push_str(", world!");
 }
 
-fn makes_copy(some_integer: i32) {
-    println!("{}", some_integer);
-}
+
 
 // 2-3 所有权与借用 Tips
-// 2-3-1 所有权
+// 2-3-2 引用与借用
 
-// ⭐️编程语言释放内存的流派
-// 垃圾回收机制（GC） - Java Go
-// 手动管理内存的分配和释放 C++
-// 通过所有权来管理内存 Rust
+// ⭐️⭐️ 引用与解引用
+// ⭐️引用 let y = &x;
+// ⭐️解引用 assert_eq!(5, *y);
 
-// ※※谨记※※
-// ⭐️所有权规则
-// 1. Rust中每一个值都被一个变量所拥有，该变量被称为值的所有者
-// 2. 一个值同时只能被一个变量所拥有，或者说一个值只能拥有一个所有者
-// 3. 当所有者(变量)离开作用域范围时，这个值将被丢弃(drop)
+// ⭐️⭐️ 不可变引用
+// 引用指向的值默认是不可变的
+
+// ⭐️⭐️ 可变引用
+// 首先，声明 s 是可变类型，其次创建一个可变的引用 &mut s 和接受可变引用参数 some_string: &mut String 的函数
+// 可变引用同时只能存在一个
